12586d5e6e0cfd91baa4b45df875242d0e32f2ab
ahadsidtechno <uzairahadmagsi0@gmail.com>
2025-01-31 17:51:51 +0500

    first commit

---
+++ src/index.js
@@ -0,0 +1,3 @@
+export { default as Skeleton } from "./components/Skeleton";
+export { default as SkeletonGroup } from "./components/SkeletonGroup";
+export { default as ContentLoader } from "./components/ContentLoader";
